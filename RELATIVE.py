299c5f9a263a1c34f947bb9943483f5f71157ec4
Aaditya Rasala <66347812+aadityarasala@users.noreply.github.com>
2021-10-30 11:00:42 +0530

    Create RELATIVE.py

---
+++ RELATIVE.py
@@ -0,0 +1,7 @@
+#Relativity Problem Code: RELATIVE
+T = int(input()) #Total number of testcase
+for i in range(T):
+    g,c = map(int, input().split()) #define saperate elements
+    v = c*c
+    cheight = int(v/(2*g))
+    print(cheight)
