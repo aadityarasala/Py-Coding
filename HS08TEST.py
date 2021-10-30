edb87f57acc5de74afc309ce5df9f24c7658f8a6
Aaditya Rasala <66347812+aadityarasala@users.noreply.github.com>
2021-10-30 11:03:36 +0530

    Create HS08TEST.py

---
+++ HS08TEST.py
@@ -0,0 +1,10 @@
+#ATM Problem Code: HS08TEST
+try:
+    cash,balance = map(float, input().split())
+    if cash <= balance-0.5 and cash % 5==0:
+        print("%.2f"%(balance-cash-0.50))
+    else:
+        print("%.2f"%balance)
+    
+except:
+    pass
